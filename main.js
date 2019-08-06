cf0f4b0c1a0325b2d982be9f7e4f674137dc1a5e
Jorge Alan <jorge_951-ck@hotmail.com>
2019-08-06 14:29:44 -0500

    se añadio filtro json

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+let $ = require('cheerio');
+
+let filters = {
+    country: undefined, //array de strings con dos caracteres de longitud
+    proxySpeed: undefined, //number
+    port: undefined, //array de numeros [0 - 65535]
+    proxyType: undefined, //array de letras [h (http), s (https), 4 (sock4), 5 (sock5)]
+    anonymity: undefined // array de numeros [1, 2, 3, 4]
+}
+
+let url = "https://hidemyna.me/en/proxy-list/";
